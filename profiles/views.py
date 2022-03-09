7013c44a36c0854c45771a322803e11eae7bdd62
DPHamdev <dphamill@gmail.com>
2022-03-09 21:25:58 +0000

    Updated views in checkout app and had to create a new superuser

--- profiles/views.py
+++ profiles/views.py
@@ -1,13 +1,15 @@
 from django.shortcuts import render, get_object_or_404
 from django.contrib import messages
+from checkout.models import Order
 from .models import UserProfile
 from .forms import UserProfileForm
 
-from checkout.models import Order
+
 
 # Create your views here.
 def profile(request):
     """ Display the user's profile. """
+   
     profile = get_object_or_404(UserProfile, user=request.user)
 
     if request.method == 'POST':
@@ -28,7 +30,7 @@ def profile(request):
 
     return render(request, template, context)
 
-    def order_history(request, order_number):
+def order_history(request, order_number):
     order = get_object_or_404(Order, order_number=order_number)
 
     messages.info(request, (
