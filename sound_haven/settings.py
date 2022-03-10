55cc3ae3327867f6ee93e648eff29c795ded7a41
DPHamdev <dphamill@gmail.com>
2022-03-10 01:14:54 +0000

    Finalising files for heroku deployment

--- sound_haven/settings.py
+++ sound_haven/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-c$9e1pd4qv+tto=moc407fxc(a2o(=)u1-ptrco6%sfv49iv4-'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
