c9cac84f1503c58133ee4bb4129e58d4597a61d3
DPHamdev <dphamill@gmail.com>
2022-03-14 00:18:06 +0000

    Add test views

--- cart/tests_views.py
+++ cart/tests_views.py
@@ -3,29 +3,29 @@ from django.urls import reverse, resolve
 from products.models import Product
 
 
-class TestBagViews(TestCase):
+class TestCartViews(TestCase):
 
     def setUp(self):
         self.client = Client()
 
     def test_url_response(self):
         """ Test URL response success """
-        response = self.client.get('/bag/')
+        response = self.client.get('/cart/')
         self.assertEqual(response.status_code, 200)
 
     def test_view_url_accessible_by_name(self):
         """ Test URL is accessible """
-        response = self.client.get(reverse('view_bag'))
+        response = self.client.get(reverse('view_cart'))
         self.assertEqual(response.status_code, 200)
 
     def test_view_uses_correct_template(self):
         """ Test using correct template """
-        response = self.client.get(reverse('view_bag'))
-        self.assertTemplateUsed(response, 'bag/bag.html')
+        response = self.client.get(reverse('view_cart'))
+        self.assertTemplateUsed(response, 'cart/cart.html')
 
 
-class TestAddToBagViews(TestCase):
-    """ Class to test the add_to_bag views """
+class TestAddToCartViews(TestCase):
+    """ Class to test the add_to_cart views """
 
     def setUp(self):
         self.client = Client()
@@ -33,32 +33,32 @@ class TestAddToBagViews(TestCase):
                                               name="testproduct",
                                               price=50.00)
         self.quantity = 1
-        self.filled_bag = {}
+        self.filled_cart = {}
 
     def test_url_exists(self):
-        """ Test view add to bag """
-        url = f'/bag/add/{self.product.id}/'
+        """ Test view add to cart """
+        url = f'/cart/add/{self.product.id}/'
         found = resolve(url)
-        self.assertEqual(found.url_name, "add_to_bag")
+        self.assertEqual(found.url_name, "add_to_cart")
 
-    def test_can_add_to_bag(self):
-        """ Test if a product can be added to bag successfully """
+    def test_can_add_to_cart(self):
+        """ Test if a product can be added to cart successfully """
         session = self.client.session
-        session['bag'] = self.filled_bag
+        session['cart'] = self.filled_bag
         session.save()
         post_data = {
-            'bag_item': self.product,
+            'cart_item': self.product,
             'quantity': int(self.quantity),
             'redirect_url': '/products/1/'
         }
-        response = self.client.post('/bag/add/1/', post_data)
+        response = self.client.post('/cart/add/1/', post_data)
         self.assertEqual(response.status_code, 302)
-        updated_bag = self.client.session.get('bag')
-        self.assertEqual(updated_bag['1'], 1)
+        updated_cart = self.client.session.get('cart')
+        self.assertEqual(updated_cart['1'], 1)
 
 
-class TestAdjustBagViews(TestCase):
-    """ Class to test the adjust_bag views """
+class TestAdjustCartViews(TestCase):
+    """ Class to test the adjust_cart views """
 
     def setUp(self):
         self.client = Client()
@@ -66,51 +66,51 @@ class TestAdjustBagViews(TestCase):
                                               name="testproduct",
                                               price=50.00)
         self.quantity = 1
-        self.filled_bag = {'1': 1, '3': 1}
+        self.filled_cart = {'1': 1, '3': 1}
 
     def test_url_exists(self):
-        """ Test view adjust bag """
-        url = f'/bag/adjust/{self.product.id}/'
+        """ Test view adjust cart """
+        url = f'/cart/adjust/{self.product.id}/'
         found = resolve(url)
-        self.assertEqual(found.url_name, "adjust_bag")
+        self.assertEqual(found.url_name, "adjust_cart")
 
-    def test_can_adjust_bag(self):
-        """ Test if the bag can be adjusted successfully """
+    def test_can_adjust_cart(self):
+        """ Test if the cart can be adjusted successfully """
         session = self.client.session
-        session['bag'] = self.filled_bag
+        session['cart'] = self.filled_bag
         session.save()
         post_data = {
-            'bag_item': self.product,
+            'cart_item': self.product,
             'quantity': int(self.quantity + 1)
         }
-        response = self.client.post('/bag/adjust/1/', post_data)
+        response = self.client.post('/cart/adjust/1/', post_data)
         self.assertEqual(response.status_code, 302)
-        updated_bag = self.client.session.get('bag')
-        self.assertEqual(updated_bag['1'], 2)
+        updated_cart = self.client.session.get('cart')
+        self.assertEqual(updated_cart['1'], 2)
 
 
-class TestRemoveFromBagViews(TestCase):
-    """ Class to test the remove_from_bag views """
+class TestRemoveFromCartViews(TestCase):
+    """ Class to test the remove_from_cart views """
 
     def setUp(self):
         self.client = Client()
         self.product = Product.objects.create(pk=1,
                                               name="testproduct",
                                               price=50.00)
-        self.filled_bag = {'1': 1, '2': 1}
+        self.filled_cart = {'1': 1, '2': 1}
 
     def test_url_exists(self):
-        """ Test view adjust bag """
-        url = f'/bag/remove/{self.product.id}/'
+        """ Test view adjust cart """
+        url = f'/cart/remove/{self.product.id}/'
         found = resolve(url)
-        self.assertEqual(found.url_name, "remove_from_bag")
+        self.assertEqual(found.url_name, "remove_from_cart")
 
-    def test_can_remove_from_bag(self):
-        """ Test if the item can be successfully removed from bag"""
+    def test_can_remove_from_cart(self):
+        """ Test if the item can be successfully removed from cart"""
         session = self.client.session
-        session['bag'] = self.filled_bag
+        session['cart'] = self.filled_cart
         session.save()
-        response = self.client.post('/bag/remove/1/')
+        response = self.client.post('/cart/remove/1/')
         self.assertEqual(response.status_code, 200)
-        updated_bag = self.client.session.get('bag')
-        self.assertEqual(len(updated_bag), 1)
+        updated_cart = self.client.session.get('cart')
+        self.assertEqual(len(updated_cart), 1)
--- products/test_views.py
+++ products/test_views.py
@@ -24,24 +24,3 @@ class TestProductsViews(TestCase):
         """ Test using correct template """
         response = self.client.get(reverse('products'))
         self.assertTemplateUsed(response, 'products/products.html')
-
-
-class TestIndividualProductViews(TestCase):
-    """ Class to test the all_products views """
-
-    def setUp(self):
-        """ Set up test data """
-        self.client = Client()
-        self.product = Product.objects.create(pk=1,
-                                              name="testproduct",
-                                              price=50.00)
-
-    def test_url_response(self):
-        """ Test URL response success """
-        response = self.client.get(f'/products/{self.product.id}/')
-        self.assertEqual(response.status_code, 200)
-
-    def test_view_uses_correct_template(self):
-        """ Test using correct template """
-        response = self.client.get(f'/products/{self.product.id}/')
-        self.assertTemplateUsed(response, 'products/product_detail.html')
