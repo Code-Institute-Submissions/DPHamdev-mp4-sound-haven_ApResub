89f926b0933e4b4b4edd309d024e444ad6bce05e
DPHamdev <dphamill@gmail.com>
2022-04-01 21:09:32 +0000

    Made new app to hopefully work with Heroku

---
+++ contacts/admin.py
@@ -0,0 +1,19 @@
+from django.contrib import admin
+from .models import Contact
+
+
+class ContactAdmin(admin.ModelAdmin):
+    """ Contact Table """
+
+    list_display = (
+        'name',
+        'email',
+        'order_number',
+        'message',
+        'date'
+    )
+    search_fields = ('name', 'email',)
+    date_hierarchy = 'date'
+
+
+admin.site.register(Contact, ContactAdmin)
---
+++ contacts/forms.py
@@ -0,0 +1,21 @@
+from django import forms
+from django.utils.translation import gettext_lazy as _
+from .models import Contact
+
+
+class ContactForm(forms.ModelForm):
+    """ Contact Form """
+
+    class Meta:
+        """ Specify model, fields, and label """
+        model = Contact
+        fields = ('name', 'email', 'order_number', 'message')
+        labels = {
+            'name': _('Full Name'),
+            'email': _('Email Address'),
+            'order_number': _('Order Number'),
+        }
+
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
