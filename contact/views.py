bac230cd7e10a9dafb8106795293538110c590b1
DPHamdev <dphamill@gmail.com>
2022-04-01 18:19:47 +0000

    Updated views on contact

--- contact/views.py
+++ contact/views.py
@@ -1,13 +1,16 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib import messages
 from .forms import ContactForm
+from .models import Contact
 
 
 def contact(request):
     """ Contact Us Page """
 
+    contact = get_object_or_404(Contact)
+
     if request.method == 'POST':
-        form = ContactForm(request.POST)
+        form = ContactForm(request.POST, instance=contact)
         if form.is_valid():
             form.save()
             messages.success(
