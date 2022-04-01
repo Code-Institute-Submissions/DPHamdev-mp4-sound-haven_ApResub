282324a3a2e9657c6739a91a531804c055c5d0f8
DPHamdev <dphamill@gmail.com>
2022-04-01 19:57:53 +0000

    Changed the model in Contact

--- contact/models.py
+++ contact/models.py
@@ -11,4 +11,4 @@ class Contact(models.Model):
     date = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return self.name + "-" +  self.email
+        return self.name + "-" + self.email
