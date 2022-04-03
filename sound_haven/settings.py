2d0208ea283051d004af2403939fc14fa1162c0c
DPHamdev <dphamill@gmail.com>
2022-04-03 12:11:30 +0000

    Changed DEBUG in settings, and formatting of blog posts on blogs.html

--- sound_haven/settings.py
+++ sound_haven/settings.py
@@ -25,9 +25,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
-# 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['sound-haven.herokuapp.com', 'localhost']
 
